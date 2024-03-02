d1d6ab1ffda15ed4564923620b0a627f580971e6
joeyglasser <josephmglasser@gmail.com>
2024-03-02 00:50:25 -0500

    added initial map generation

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
 import "./App.css";
 import { Map } from "./features/map/Map";
 import { Control } from "./features/map/Control";
-import { useSelector } from "react-redux";
+import { generateMap } from "./features/map/mapSlice";
+import { useSelector, useDispatch } from "react-redux";
 
 import "./styles/css/styles.css";
 import { useMediaQuery } from "react-responsive";
 
 function App() {
-  let map = Map();
+  const dispatch = useDispatch();
+  dispatch(generateMap(30, 30, 1, 20, 2));
+
   let nav_width = useSelector((state) => state.map.nav_width);
   const smallWidth = useMediaQuery({ query: "(max-width: 1224px)" });
   const portrait = useMediaQuery({ query: "orientation: portrait" });
